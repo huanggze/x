e51e860e9211706fac1dbc3b7c7abc0571caf951
黄广喆 <huangguangzhe@corp.netease.com>
2025-07-12 22:18:14 +0800

    update contextx

--- contextx/default.go
+++ contextx/default.go
@@ -1,3 +1,15 @@
 package contextx
 
+import (
+	"context"
+
+	"github.com/huanggze/x/configx"
+)
+
 type Default struct{}
+
+var _ Contextualizer = (*Default)(nil)
+
+func (d *Default) Config(ctx context.Context, config *configx.Provider) *configx.Provider {
+	return config
+}
