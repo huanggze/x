ebcdf43041f82aac66a4987afa46cfa278762987
黄广喆 <huangguangzhe@corp.netease.com>
2025-07-13 19:25:25 +0800

    update configx

---
+++ configx/helpers.go
@@ -0,0 +1,8 @@
+package configx
+
+import "github.com/spf13/pflag"
+
+// RegisterFlags registers the config file flag.
+func RegisterFlags(flags *pflag.FlagSet) {
+	flags.StringSliceP("config", "c", []string{}, "Path to one or more .json, .yaml, .yml, .toml config files. Values are loaded in the order provided, meaning that the last config file overwrites values from the previous config file.")
+}
