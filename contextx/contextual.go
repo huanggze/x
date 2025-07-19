0213d44f4471092d49194b1205cd451a24c2f828
黄广喆 <huangguangzhe@corp.netease.com>
2025-07-19 23:19:18 +0800

    add networkx

--- contextx/contextual.go
+++ contextx/contextual.go
@@ -3,12 +3,19 @@ package contextx
 import (
 	"context"
 
+	"github.com/gofrs/uuid"
+
 	"github.com/huanggze/x/configx"
 )
 
 type (
 	Contextualizer interface {
+		// Network returns the network id for the given context.
+		Network(ctx context.Context, network uuid.UUID) uuid.UUID
 		// Config returns the config for the given context.
 		Config(ctx context.Context, config *configx.Provider) *configx.Provider
+	}
+	Provider interface {
+		Contextualizer() Contextualizer
 	}
 )
--- networkx/manager.go
+++ networkx/manager.go
@@ -1,9 +1,52 @@
 package networkx
 
-import "embed"
+import (
+	"context"
+	"embed"
+	"errors"
+
+	"github.com/huanggze/x/logrusx"
+	"github.com/huanggze/x/otelx"
+	"github.com/huanggze/x/sqlcon"
+	"github.com/ory/pop/v6"
+)
 
 // Migrations of the network manager. Apply by merging with your local migrations using
 // fsx.Merge() and then passing all to the migration box.
 //
 //go:embed migrations/sql/*.sql
 var Migrations embed.FS
+
+type Manager struct {
+	c *pop.Connection
+	l *logrusx.Logger
+	t *otelx.Tracer
+}
+
+func NewManager(
+	c *pop.Connection,
+	l *logrusx.Logger,
+	t *otelx.Tracer,
+) *Manager {
+	return &Manager{
+		c: c,
+		l: l,
+		t: t,
+	}
+}
+
+func (m *Manager) Determine(ctx context.Context) (*Network, error) {
+	var p Network
+	c := m.c.WithContext(ctx)
+	if err := sqlcon.HandleError(c.Q().Order("created_at ASC").First(&p)); err != nil {
+		if errors.Is(err, sqlcon.ErrNoRows) {
+			np := NewNetwork()
+			if err := c.Create(np); err != nil {
+				return nil, err
+			}
+			return np, nil
+		}
+		return nil, err
+	}
+	return &p, nil
+}
