ec0be307ffa5238d8f005e172c5c2e4b791533ba
黄广喆 <huangguangzhe@corp.netease.com>
2025-08-08 17:50:22 +0800

    update sqlxx

--- sqlxx/types.go
+++ sqlxx/types.go
@@ -1,5 +1,10 @@
 package sqlxx
 
+import "encoding/json"
+
 // swagger:type string
 // swagger:model nullString
 type NullString string
+
+// JSONRawMessage represents a json.RawMessage that works well with JSON, SQL, and Swagger.
+type JSONRawMessage json.RawMessage
