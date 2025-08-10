c044e23fcbd109095f8fba58d71bc3e033b328dd
黄广喆 <huangguangzhe@corp.netease.com>
2025-08-11 00:34:00 +0800

    update

--- popx/transaction.go
+++ popx/transaction.go
@@ -2,6 +2,10 @@ package popx
 
 import (
 	"context"
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
+	"github.com/jmoiron/sqlx"
+	"github.com/prometheus/client_golang/prometheus"
+	"runtime"
 
 	"github.com/ory/pop/v6"
 )
@@ -14,6 +18,38 @@ func WithTransaction(ctx context.Context, tx *pop.Connection) context.Context {
 	return context.WithValue(ctx, transactionKey, tx)
 }
 
+func Transaction(ctx context.Context, connection *pop.Connection, callback func(context.Context, *pop.Connection) error) error {
+	c := ctx.Value(transactionKey)
+	if c != nil {
+		if conn, ok := c.(*pop.Connection); ok {
+			return callback(ctx, conn.WithContext(ctx))
+		}
+	}
+
+	if connection.Dialect.Name() == "cockroach" {
+		return connection.WithContext(ctx).Dialect.Lock(func() error {
+			transaction, err := connection.NewTransaction()
+			if err != nil {
+				return err
+			}
+
+			attempt := 0
+			return crdb.ExecuteInTx(ctx, sqlxTxAdapter{transaction.TX.Tx}, func() error {
+				attempt++
+				if attempt > 1 {
+					caller := caller()
+					transactionRetries.WithLabelValues(caller).Inc()
+				}
+				return callback(WithTransaction(ctx, transaction), transaction)
+			})
+		})
+	}
+
+	return connection.WithContext(ctx).Transaction(func(tx *pop.Connection) error {
+		return callback(WithTransaction(ctx, tx), tx)
+	})
+}
+
 func GetConnection(ctx context.Context, connection *pop.Connection) *pop.Connection {
 	c := ctx.Value(transactionKey)
 	if c != nil {
@@ -22,4 +58,54 @@ func GetConnection(ctx context.Context, connection *pop.Connection) *pop.Connect
 		}
 	}
 	return connection.WithContext(ctx)
+}
+
+type sqlxTxAdapter struct {
+	*sqlx.Tx
+}
+
+var _ crdb.Tx = sqlxTxAdapter{}
+
+func (s sqlxTxAdapter) Exec(ctx context.Context, query string, args ...interface{}) error {
+	_, err := s.Tx.ExecContext(ctx, query, args...)
+	return err
+}
+
+func (s sqlxTxAdapter) Commit(ctx context.Context) error {
+	return s.Tx.Commit()
+}
+
+func (s sqlxTxAdapter) Rollback(ctx context.Context) error {
+	return s.Tx.Rollback()
+}
+
+var (
+	transactionRetries = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "ory_x_popx_cockroach_transaction_retries_total",
+		Help: "Counts the number of automatic CockroachDB transaction retries",
+	}, []string{"caller"})
+	unknownCaller = "unknown"
+)
+
+func caller() string {
+	pc := make([]uintptr, 3)
+	// The number stack frames to skip was determined by putting a breakpoint in
+	// ory/kratos and looking for the topmost frame which isn't from ory/x or
+	// ory/pop.
+	n := runtime.Callers(8, pc)
+	if n == 0 {
+		return unknownCaller
+	}
+	pc = pc[:n]
+	frames := runtime.CallersFrames(pc)
+	for {
+		frame, more := frames.Next()
+		if frame.Function != "" {
+			return frame.Function
+		}
+		if !more {
+			break
+		}
+	}
+	return unknownCaller
 }
