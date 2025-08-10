8fde97e8ed0c793dc4183d7f79f852f54fe21bfd
黄广喆 <huangguangzhe@corp.netease.com>
2025-08-10 21:19:42 +0800

    update dbal

--- dbal/dsn.go
+++ dbal/dsn.go
@@ -1,6 +1,9 @@
 package dbal
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+)
 
 var dsnRegex = regexp.MustCompile(`^(sqlite://file:(?:.+)\?((\w+=\w+)(&\w+=\w+)*)?(&?mode=memory)(&\w+=\w+)*)$|(?:sqlite://(file:)?:memory:(?:\?\w+=\w+)?(?:&\w+=\w+)*)|^(?:(?::memory:)|(?:memory))$`)
 
@@ -13,4 +16,18 @@ var dsnRegex = regexp.MustCompile(`^(sqlite://file:(?:.+)\?((\w+=\w+)(&\w+=\w+)*
 // see: https://sqlite.org/inmemorydb.html
 func IsMemorySQLite(dsn string) bool {
 	return dsnRegex.MatchString(dsn)
+}
+
+// NewSQLiteTestDatabase creates a new unique SQLite database
+// which is shared amongst all callers and identified by an individual file name.
+func NewSQLiteTestDatabase(t interface {
+	TempDir() string
+}) string {
+	return NewSQLiteInMemoryDatabase(t.TempDir())
+}
+
+// NewSQLiteInMemoryDatabase creates a new unique SQLite database
+// which is shared amongst all callers and identified by an individual file name.
+func NewSQLiteInMemoryDatabase(name string) string {
+	return fmt.Sprintf("sqlite://file:%s?_fk=true&mode=memory&cache=shared", name)
 }
