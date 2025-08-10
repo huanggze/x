2ad756b725d40f60680bcc3a016d305ef3117a60
黄广喆 <huangguangzhe@corp.netease.com>
2025-08-10 21:36:49 +0800

    update jsonnetsecure

---
+++ jsonnetsecure/jsonnet.go
@@ -0,0 +1,35 @@
+package jsonnetsecure
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+	"testing"
+)
+
+func JsonnetTestBinary(t testing.TB) string {
+	t.Helper()
+
+	// We can force the usage of a given jsonnet executable.
+	// Useful to test different versions, or run the tests under wine.
+	if s := os.Getenv("ORY_JSONNET_PATH"); s != "" {
+		return s
+	}
+
+	var stderr bytes.Buffer
+	// Using `t.TempDir()` results in permissions errors on Windows, sometimes.
+	outPath := path.Join(os.TempDir(), "jsonnet")
+	if runtime.GOOS == "windows" {
+		outPath = outPath + ".exe"
+	}
+	cmd := exec.Command("go", "build", "-o", outPath, "github.com/ory/x/jsonnetsecure/cmd")
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil || stderr.Len() != 0 {
+		t.Fatalf("building the Go binary returned error: %v\n%s", err, stderr.String())
+	}
+
+	return outPath
+}
--- jsonnetsecure/provider.go
+++ jsonnetsecure/provider.go
@@ -1,6 +1,24 @@
 package jsonnetsecure
 
+import (
+	"runtime"
+	"testing"
+)
+
 type (
 	VMProvider interface {
 	}
+
+	// TestProvider provides a secure VM by running go build on github.
+	// com/ory/x/jsonnetsecure/cmd.
+	TestProvider struct {
+		jsonnetBinary string
+		pool          Pool
+	}
 )
+
+func NewTestProvider(t testing.TB) *TestProvider {
+	pool := NewProcessPool(runtime.GOMAXPROCS(0))
+	t.Cleanup(pool.Close)
+	return &TestProvider{JsonnetTestBinary(t), pool}
+}
