fe29a33efe24d10e2d166e204701e06ba7afbdba
黄广喆 <huangguangzhe@corp.netease.com>
2025-08-11 22:50:18 +0800

    update dbal

---
+++ dbal/canonicalize.go
@@ -0,0 +1,15 @@
+package dbal
+
+const (
+	// DriverMySQL is the mysql driver name.
+	DriverMySQL = "mysql"
+
+	// DriverPostgreSQL is the postgres driver name.
+	DriverPostgreSQL = "postgres"
+
+	// DriverCockroachDB is the cockroach driver name.
+	DriverCockroachDB = "cockroach"
+
+	// UnknownDriver is the driver name if the driver is unknown.
+	UnknownDriver = "unknown"
+)
