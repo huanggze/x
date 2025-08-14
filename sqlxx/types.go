84624c2bc442180bd7ea9db08c8ec489b6837edf
黄广喆 <huangguangzhe@corp.netease.com>
2025-08-15 00:26:41 +0800

    update sqlxx

--- sqlxx/types.go
+++ sqlxx/types.go
@@ -1,6 +1,8 @@
 package sqlxx
 
 import (
+	"database/sql"
+	"database/sql/driver"
 	"encoding/json"
 	"time"
 )
@@ -14,6 +16,21 @@ type NullString string
 // swagger:model nullTime
 // required: false
 type NullTime time.Time
+
+// Scan implements the Scanner interface.
+func (ns *NullTime) Scan(value interface{}) error {
+	var v sql.NullTime
+	if err := (&v).Scan(value); err != nil {
+		return err
+	}
+	*ns = NullTime(v.Time)
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (ns NullTime) Value() (driver.Value, error) {
+	return sql.NullTime{Valid: !time.Time(ns).IsZero(), Time: time.Time(ns)}.Value()
+}
 
 // JSONRawMessage represents a json.RawMessage that works well with JSON, SQL, and Swagger.
 type JSONRawMessage json.RawMessage
